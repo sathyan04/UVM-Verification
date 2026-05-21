fd60f100c066fa19ba0d0de2650fbda43a6bd568
Sathyan S S <sssathyan2004@gmail.com>
2026-05-21 11:39:06 +0530

    Create 08_Agent.sv

---
+++ 01. Half Adder/08_Agent.sv
@@ -0,0 +1,27 @@
+class my_agent extends uvm_agent;
+  `uvm_component_utils(my_agent);
+  
+  my_sequencer	sqr;
+  my_driver		dri;
+  my_monitor	mon;
+  uvm_analysis_port #(my_transaction) ap;
+  
+  function new(string name = "my_agent",uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    sqr = my_sequencer::type_id::create("sqr",this);
+    dri = my_driver::type_id::create("dri",this);
+    mon = my_monitor::type_id::create("mon",this);
+    ap	= new("ap",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    dri.seq_item_port.connect(sqr.seq_item_export);
+    mon.ap.connect(ap);
+  endfunction
+  
+endclass	
