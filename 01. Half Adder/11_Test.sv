017e541870b498107703a06a088228922d5a6e89
Sathyan S S <sssathyan2004@gmail.com>
2026-05-21 11:40:58 +0530

    Create 11_Test.sv

---
+++ 01. Half Adder/11_Test.sv
@@ -0,0 +1,23 @@
+class my_test extends uvm_test;
+  `uvm_component_utils(my_test);
+  my_env env;
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = my_env::type_id::create("my_env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    my_sequence seq;
+    phase.raise_objection(this);
+    seq = my_sequence::type_id::create("seq");
+    seq.start(env.agt.sqr);
+    #20;
+    phase.drop_objection(this);
+  endtask
+
+endclass
